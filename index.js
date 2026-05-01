abe9b4ce0eb241da7fafc56d10ba7dd6f8d68629
felcy2303 <felcyrovia@gmail.com>
2026-05-01 16:54:57 +0530

    Enhance CORS settings in Express app
    Updated CORS configuration to allow all origins and specified allowed methods.

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/todos", require("./routes/todo"));
